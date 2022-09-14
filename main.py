017080835441b30c165bc29df3e7a43aa4934151
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2022-09-14 13:12:51 -0400

    Business Push
    Included Business Page

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def dashboard():
     return render_template('index.html')
 
-@app.route('/software')
+@app.route('/business')
 def login():
-    return render_template('software.html')
+    return render_template('business.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
